e9d37cd50ca5e6c33cfdbf02f4b009e8c1bb1971
stacysn <stacysuen5@gmail.com>
2018-02-13 16:49:46 -0800

    included non-functioning add article btn

--- src/Head/Header.js
+++ src/Head/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
 
 
 
@@ -9,6 +11,12 @@ class Header extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Get Pocket</h1>
+          <FormGroup >
+           <Label >
+             <Input type="text" />
+           </Label>
+         </FormGroup>
+         <Button>Add Article</Button>
         </header>
       </div>
     )
