9e1461e94524729b6374ea568d6e8926c9e62b32
stacysn <stacysuen5@gmail.com>
2018-02-13 16:35:17 -0800

    passed in title of hardcoded articles

---
+++ src/Head/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Get Pocket</h1>
+        </header>
+      </div>
+    )
+ }
+}
+
+export default Header;
