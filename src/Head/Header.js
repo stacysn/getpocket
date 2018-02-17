fe4d024b3b84cf7fa62c894c3202d86992656ec5
stacysn <stacysuen5@gmail.com>
2018-02-17 10:44:41 -0800

    moved add article out of header

--- src/Head/Header.js
+++ src/Head/Header.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { Button, FormGroup, Label, Input } from 'reactstrap';
 
 class Header extends Component {
   render() {
@@ -8,16 +7,6 @@ class Header extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Get Pocket</h1>
-          <FormGroup >
-           <Label >
-             <Input
-               type="text"
-               value={this.props.newArticle}
-               onChange={(e) => this.props.addArticle(e)}
-              />
-           </Label>
-         </FormGroup>
-         <Button type="submit" style={{padding: '0.1rem'}} onClick={(e) => this.props.addArticle(e)}> Add Article</Button>
         </header>
       </div>
     )
